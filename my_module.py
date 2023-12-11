4e4aa9332eafa4e99e8817be1c0ed2282bee60a8
Maxime JACQUOT <107543514+PxMaax@users.noreply.github.com>
2023-12-11 16:14:15 +0100

    Update my_module.py

--- my_module.py
+++ my_module.py
@@ -1,5 +1,7 @@
 # Fichier: my_module.py
-
+"""
+Ce module contient la fonction add_one qui ajoute 1 à un nombre donné.
+"""
 def add_one(number):
     """
     Ajoute 1 à un nombre et retourne le résultat.
